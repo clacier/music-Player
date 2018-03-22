9ad1f9bc132e740b511de1b967f552cf5ac3ea83
clacier <31944865+clacier@users.noreply.github.com>
2018-03-22 12:07:36 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -410,7 +410,7 @@ $(function(){
 
 
 	//缓冲加载条
-	var startBuffer=function(){
+	/*var startBuffer=function(){
 		var maxduration=video[0].duration;
 		var currentBuffer=video[0].buffered.end(0);
 		var percentage=100*currentBuffer/maxduration;
@@ -422,7 +422,7 @@ $(function(){
 	};
 	setTimeout(startBuffer,500);
 
-
+*/
 	//静音按钮
 	$(".muted").click(function(){
 		if(video[0].muted){
